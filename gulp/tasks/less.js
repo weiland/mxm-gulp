9b7aba4d026b7624305012c85cf1ddc708be07b8
tuelsch <tuelsch@gmail.com>
2015-02-13 10:38:02 +0100

    Reorganize tasks

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -3,7 +3,6 @@ var less                 = require('gulp-less');
 var using                = require('gulp-using');
 var concat               = require('gulp-concat');
 var cached               = require('gulp-cached');
-var changed              = require('gulp-changed');
 var progeny              = require('gulp-progeny');
 var remember             = require('gulp-remember');
 var sourcemaps           = require('gulp-sourcemaps');
@@ -11,49 +10,44 @@ var options              = require('../options').less;
 var lessPluginAutoprefix = require('less-plugin-autoprefix');
 var lessPluginCleanCSS   = require('less-plugin-clean-css');
 
-var browsers = [
-	'Android >= 2.3',
-	'Chrome >= 20',
-	'Firefox >= 24', // Firefox 24 is the latest ESR
-	'Explorer >= 8',
-	'iOS >= 6',
-	'Opera >= 12',
-	'Safari >= 6'
-];
-var autoprefix = new lessPluginAutoprefix({browsers: browsers});
-var cleancss = new lessPluginCleanCSS({});
+var autoprefix = new lessPluginAutoprefix({browsers: options.browsers});
+var cleancss = new lessPluginCleanCSS({advanced: true});
 
 // Default task
 gulp.task('default', ['less', 'watch']);
 
 // Build task
 gulp.task('less', function () {
-	return setTimeout(function () {
-	gulp.src(options.src)
-		.pipe(cached('less'))
-		.pipe(using({prefix: 'input'}))
-		.pipe(progeny({
-            regexp: /^\s*@import\s*(?:\(\w+\)\s*)?['"]([^'"]+)['"]/
-        }))
-		.pipe(sourcemaps.init())
-		.pipe(less({plugins:[autoprefix, cleancss]}))
-		.on('error', function (err) { console.log(err); })
-		.pipe(using({prefix: 'compiled'}))
-		.pipe(remember('css'))
-		.pipe(concat('main.css'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(options.dest));
-	}, 20);
-
+	return setTimeout(build, 20);
 });
 
 // Watcher task
 gulp.task('watch', function () {
-	var watcher = gulp.watch(glob, ['less']);
-	watcher.on('change', function (e) {
-		if (e.type === 'deleted') {
-			delete cached.caches.scripts[e.path];
-			remember.forget('css', e.path);
-		}
-	});
+	gulp.watch(glob, ['less'])
+		.on('change', change);
 });
+
+function build () {
+	gulp.src(options.src)
+		.pipe(cached('less'))
+		.pipe(progeny({
+            regexp: /^\s*@import\s*(?:\(\w+\)\s*)?['"]([^'"]+)['"]/
+        }))
+		.pipe(sourcemaps.init())
+		.pipe(less({
+			plugins:[autoprefix, cleancss]
+		}))
+		.on('error', function (err) { console.log(err); })
+		.pipe(remember('css'))
+		.pipe(concat('main.css'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(options.dest));
+}
+
+function change (e) {
+	console.log('watcher ' + e.type);
+	if (e.type === 'deleted') {
+		delete cached.caches.scripts[e.path];
+		remember.forget('css', e.path);
+	}
+}
